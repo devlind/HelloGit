d4b9cfc47e97a1bcc664727815b3f68f5c4da88e
devlind <devlin.dunsmore@gmail.com>
2012-06-19 21:55:35 -0700

    committing change to mybranch to be pulled into master

--- src/com/hello/git/MyBranch.java
+++ src/com/hello/git/MyBranch.java
@@ -5,7 +5,7 @@ public class MyBranch {
 	public static void main(String[] args) {
 		System.out.println("This was added in the \"mybranch\" branch");
 		
-		System.out.println("This will be committed in mybranch and pulled into master");
+		System.out.println("This will be committed in mybranch and merged into master");
 	}
 	
 }
