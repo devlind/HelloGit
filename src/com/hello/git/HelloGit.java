f054293a3bf952d6f70f1c7d39f8e03ae6af8c06
devlind <devlin.dunsmore@gmail.com>
2012-06-21 23:48:12 -0700

    giving this a few more tries and merging with parallel master changes

--- src/com/hello/git/HelloGit.java
+++ src/com/hello/git/HelloGit.java
@@ -3,6 +3,11 @@ package com.hello.git;
 public class HelloGit {
 
 	public static void main(String[] args) {
+		System.out.println("giving rebasing and branching another try...");
+		System.out.println("and another try");
+		System.out.println("and yet another...");
+		
+		
 		System.out.println("Hello Git!!");
 		System.out.println("Added this change in master only");
 
@@ -10,29 +15,23 @@ public class HelloGit {
 		System.out.println("Rebase change #1");
 		System.out.println("Rebase change #2");
 		System.out.println("Rebase change #3");
-		
 		System.out.println("Rebase change #4");
 		System.out.println("Rebase change #5");
 		System.out.println("Rebase change #6");
-		
 		System.out.println("Rebase change #7");
 		System.out.println("Rebase change #8");
 		System.out.println("Rebase change #9");
-		
 		System.out.println("Rebase change #10");
 		System.out.println("Rebase change #11");
 		System.out.println("Rebase change #12");
 		System.out.println("Another change happening at the same time");
-		
 		System.out.println("Made changes in parallel with rebase 7, 8, 9");
-		
 		System.out.println("Rebase change #13");
-		
 		System.out.println("another change #1");
-		
 		System.out.println("another change #2");
-		
 		System.out.println("another change #3");
+		
+		System.out.println("meanwhile in master....");
 	}
 	
 }
