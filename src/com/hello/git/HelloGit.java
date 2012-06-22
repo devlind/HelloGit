cf797fb776f35b20bd9748c5680b5bc45f1f4d02
devlind <devlin.dunsmore@gmail.com>
2012-06-21 21:21:22 -0700

    Added new change after rebase_branch changes

--- src/com/hello/git/HelloGit.java
+++ src/com/hello/git/HelloGit.java
@@ -5,6 +5,8 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!!");
 		System.out.println("Added this change in master only");
+		
+		System.out.println("Added this change after rebase_branch commits");
 	}
 	
 }
